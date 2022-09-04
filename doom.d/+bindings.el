b28c48e54041af26c70cbb29e227028168314972
Igor Anic <ianic@inet.hr>
2022-09-04 11:10:50 +0200

    add all macos shortucts to linux emacs

--- doom.d/+bindings.el
+++ doom.d/+bindings.el
@@ -38,7 +38,57 @@
  "C-c C-;"      #'comment-dwim
  "s-f"          #'forward-word
  "s-b"          #'backward-word
+ ;; copy paste for ubuntu
+ "s-z"          #'undo-fu-only-undo
+ "s-x"          #'kill-region
+ "s-c"          #'copy-region-as-kill
+ "s-v"          #'yank
+ "s-a"          #'mark-whole-buffer
 )
+
+;; macos specific keybindings
+;; from: https://github.com/doomemacs/doomemacs/blob/c44bc81a05f3758ceaa28921dd9c830b9c571e61/modules/config/default/config.el#L298
+
+  ;; Fix MacOS shift+tab
+  (define-key key-translation-map [S-iso-lefttab] [backtab])
+  ;; Fix conventional OS keys in Emacs
+  (map! "s-`" #'other-frame  ; fix frame-switching
+        ;; fix OS window/frame navigation/manipulation keys
+        "s-w" #'delete-window
+        "s-W" #'delete-frame
+        "s-n" #'+default/new-buffer
+        "s-N" #'make-frame
+        "s-q" (if (daemonp) #'delete-frame #'save-buffers-kill-terminal)
+        "C-s-f" #'toggle-frame-fullscreen
+        ;; Restore somewhat common navigation
+        "s-l" #'goto-line
+        ;; Restore OS undo, save, copy, & paste keys (without cua-mode, because
+        ;; it imposes some other functionality and overhead we don't need)
+        "s-f" (if (modulep! :completion vertico) #'consult-line #'swiper)
+        "s-z" #'undo
+        "s-Z" #'redo
+        "s-c" (if (featurep 'evil) #'evil-yank #'copy-region-as-kill)
+        "s-v" #'yank
+        "s-s" #'save-buffer
+        "s-x" #'execute-extended-command
+        :v "s-x" #'kill-region
+        ;; Buffer-local font scaling
+        "s-+" #'doom/reset-font-size
+        "s-0" #'doom/reset-font-size
+        "s-=" #'doom/increase-font-size
+        "s--" #'doom/decrease-font-size
+        ;; Conventional text-editing keys & motions
+        "s-a" #'mark-whole-buffer
+        "s-/" (cmd! (save-excursion (comment-line 1)))
+        :n "s-/" #'evilnc-comment-or-uncomment-lines
+        :v "s-/" #'evilnc-comment-operator
+        :gi  [s-backspace] #'doom/backward-kill-to-bol-and-indent
+        :gi  [s-left]      #'doom/backward-to-bol-or-indent
+        :gi  [s-right]     #'doom/forward-to-last-non-comment-or-eol
+        :gi  [M-backspace] #'backward-kill-word
+        :gi  [M-left]      #'backward-word
+        :gi  [M-right]     #'forward-word)
+
 
 ;; Go hooks
 (defun lsp-go-install-save-hooks ()
@@ -95,3 +145,6 @@
 (map! :localleader
       :map zig-mode-map
       "m" #'zig-build-flash )
+
+
+;;(set-face-attribute 'default nil :height 192)
