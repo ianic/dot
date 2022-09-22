e1e4ce523fee8e593f455e9f7b12a14e42c02886
Igor Anić <igor.anic@gmail.com>
2022-09-22 15:22:22 +0200

    llvm without sudo

--- macos/build_zig.sh
+++ macos/build_zig.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 #
 #Ref: https://www.epmor.app/posts/building-zig-from-scratch
 
@@ -6,15 +6,20 @@ if [[ $(brew list | grep -E "ninja|cmake" -c) != 2 ]]; then
     brew install cmake ninja
 fi
 
-llvm_install_dir=/opt/llvm-macos13.0-arm64-15.0.0-release
+build_name=llvm-macos$(sw_vers -productVersion)-arm64-15.0.0-release
+llvm_install_dir=$(pwd)/$build_name
 
 if [ ! -d $llvm_install_dir ]; then
-   git clone https://github.com/llvm/llvm-project
-   cd llvm-project
-   git checkout llvmorg-15.0.0
-   cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/llvm-macos13.0-arm64-15.0.0-release -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_ENABLE_PROJECTS='clang;lld' -DLLVM_ENABLE_PLUGINS=OFF -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_INCLUDE_TESTS=OFF -S ./llvm -B _build.llvm-macos13.0-arm64-15.0.0-release
-   ninja -C _build.llvm-macos13.0-arm64-15.0.0-release
-   sudo ninja install -C _build.llvm-macos13.0-arm64-15.0.0-release
+   if [ ! -d llvm-project ]; then
+       git clone https://github.com/llvm/llvm-project
+       cd llvm-project
+       git checkout llvmorg-15.0.0
+   else
+       cd llvm-project
+   fi
+   cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$llvm_install_dir -DLLVM_PARALLEL_LINK_JOBS=8 -DLLVM_ENABLE_PROJECTS='clang;lld' -DLLVM_ENABLE_PLUGINS=OFF -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_INCLUDE_TESTS=OFF -S ./llvm -B _build.$build_name
+   ninja -C _build.$build_name
+   ninja install -C _build.$build_name
    cd  ..
 fi
 
@@ -22,6 +27,8 @@ zig_build() {
     cmake -G Ninja -B stage3 -DCMAKE_PREFIX_PATH=$llvm_install_dir
     cd stage3
     ninja
+
+    echo "#(pwd)stage3/bin/zig"
     stage3/bin/zig version
 }
 
@@ -39,3 +46,11 @@ else
         zig_build
     fi
 fi
+
+
+# notes:
+# xcrun --show-sdk-version
+# xcrun --show-sdk-path
+# clang --version
+# system_profiler SPSoftwareDataType
+# sw_vers -productVersion
