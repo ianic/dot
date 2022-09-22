400bd670fef0e5ea244faa17a8d84e38576780f8
Igor Anić <igor.anic@gmail.com>
2022-09-22 17:00:48 +0200

    create macos system Brewfile

--- macos/setup.sh
+++ macos/setup.sh
@@ -2,9 +2,21 @@
 #
 #
 
+# install all homebrew packages
+brew bundle --file Brewfile
+
+# Ref: https://developer.apple.com/forums/thread/665661
 if [ ! -f ~/Library/LaunchAgents/gnu.emacs.daemon.plist ]; then
     ln -s ~/code/dot/macos/gnu.emacs.daemon.plist ~/Library/LaunchAgents
 
     launchctl load -w ~/Library/LaunchAgents/gnu.emacs.daemon.plist
     launchctl start gnu.emacs.daemon
 fi
+
+
+
+# notes
+# generate Brewfile from current system
+# brew bundle dump
+# install all packages from Brewfile
+# brew bundle
