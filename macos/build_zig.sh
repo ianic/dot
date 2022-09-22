c57f5080b1221f572315459b7e1e8787213e8d1f
Igor Anić <igor.anic@gmail.com>
2022-09-22 13:50:20 +0200

    use brew njinja enable rebuild

--- macos/build_zig.sh
+++ macos/build_zig.sh
@@ -2,33 +2,35 @@
 #
 #Ref: https://www.epmor.app/posts/building-zig-from-scratch
 
-brew install cmake
+brew install cmake ninja
 
-# ninja build from source
-git clone git@github.com:ninja-build/ninja.git
-cd ninja
-cmake -Bbuild-cmake
-cmake --build build-cmake
-export PATH=$PATH:$(pwd)/build-cmake
-ninja --version
-cd ..
+llvm_install_dir=/opt/llvm-macos13.0-arm64-15.0.0-release
 
-git clone https://github.com/llvm/llvm-project
-cd llvm-project
-git checkout llvmorg-15.0.0
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/llvm-macos13.0-arm64-15.0.0-release -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_ENABLE_PROJECTS='clang;lld' -DLLVM_ENABLE_PLUGINS=OFF -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_INCLUDE_TESTS=OFF -S ./llvm -B _build.llvm-macos13.0-arm64-15.0.0-release
-ninja -C _build.llvm-macos13.0-arm64-15.0.0-release
-sudo ninja install -C _build.llvm-macos13.0-arm64-15.0.0-release
-export PATH=$PATH:/opt/llvm-macos13.0-arm64-15.0.0-release
-cd  ..
+if [ ! -d $llvm_install_dir ]; then
+   git clone https://github.com/llvm/llvm-project
+   cd llvm-project
+   git checkout llvmorg-15.0.0
+   cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/llvm-macos13.0-arm64-15.0.0-release -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_ENABLE_PROJECTS='clang;lld' -DLLVM_ENABLE_PLUGINS=OFF -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_INCLUDE_TESTS=OFF -S ./llvm -B _build.llvm-macos13.0-arm64-15.0.0-release
+   ninja -C _build.llvm-macos13.0-arm64-15.0.0-release
+   sudo ninja install -C _build.llvm-macos13.0-arm64-15.0.0-release
+   cd  ..
+fi
+export PATH=$llvm_install_dir:$PATH
 
-git clone https://github.com/ziglang/zig
-cd zig
-cmake -G Ninja -B stage3 -DCMAKE_PREFIX_PATH=/opt/llvm-macos13.0-arm64-15.0.0-release
+if [ ! -d zig ]; then
+    git clone https://github.com/ziglang/zig
+    cd zig
+else
+    cd zig
+    git pull
+fi
+
+cmake -G Ninja -B stage3 -DCMAKE_PREFIX_PATH=$llvm_install_dir
 cd stage3
 ninja
 stage3/bin/zig version
 
+# a note to myself
 # cleanup
 # remove folder where we sarted cloning repos
 # $ sudo rm -rf /opt/llvm-macos13.0-arm64-15.0.0-release/
