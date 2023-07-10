9ff4a53911fc53703a9619d3f69bfe337eb57b22
Igor Anić <igor.anic@gmail.com>
2023-07-10 14:47:09 +0200

    more excludes for timemachine

--- macos/timemachine.sh
+++ macos/timemachine.sh
@@ -1,22 +1,28 @@
 #!/bin/bash -e
 
-cd ~/code
-
 exclude_folders() {
-  for f in $(find . -type d -name $1); do
-      if tmutil isexcluded $f | grep Excluded > /dev/null ; then
-          echo "already excluded: $f"
-          #tmutil removeexclusion $f
-      else
-          echo "excluding: $f"
-          tmutil addexclusion $f
-      fi
-  done
+    for f in $(find . -type d -name $1); do
+        if tmutil isexcluded $f | grep Excluded >/dev/null; then
+            echo "already excluded: $f"
+            #tmutil removeexclusion $f
+        else
+            echo "excluding: $f"
+            tmutil addexclusion $f
+        fi
+    done
 }
 
+cd ~/code
 exclude_folders zig-cache
 exclude_folders zig-out
 
+cd ~/zig
+exclude_folders zig-cache
+exclude_folders zig-out
+
+cd ~/src
+exclude_folders zig-cache
+exclude_folders zig-out
 
 # notes
 # defaults read /Library/Preferences/com.apple.TimeMachine SkipPaths
