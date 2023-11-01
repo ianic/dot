626764d678f592a13c6e23f511b3155d8a26caee
Igor Anić <igor.anic@gmail.com>
2023-11-01 10:33:16 +0100

    playing with multipass again

--- multipass/launch.sh
+++ multipass/launch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -ex
 #
 host=${1:-dev}
+image=${2:-22.10}
 
-multipass launch -n $host -m 4G -c 4 -d 8G 22.10
+multipass launch -n $host -m 4G -c 4 -d 8G $image
 multipass mount /Users/ianic/code $host:/home/ubuntu/code
 
 # get IP of the new host
-ip=$(multipass info $host --format json  | jq ".info.$host.ipv4[0]" -r)
+ip=$(multipass info $host --format json | jq ".info.$host.ipv4[0]" -r)
 
 # config my ssh
 cat >~/.ssh/config.d/$host <<EOF
@@ -17,14 +18,12 @@ Host $host
 EOF
 
 multipass transfer ~/.ssh/authorized_keys2 $host:/home/ubuntu/.ssh/authorized_keys2
-multipass transfer ~/.ssh/id_rsa           $host:/home/ubuntu/.ssh/id_rsa
-multipass transfer ~/.ssh/id_rsa.pub       $host:/home/ubuntu/.ssh/id_rsa.pub
+multipass transfer ~/.ssh/id_rsa $host:/home/ubuntu/.ssh/id_rsa
+multipass transfer ~/.ssh/id_rsa.pub $host:/home/ubuntu/.ssh/id_rsa.pub
 
 ssh $host 'chmod 400 ~/.ssh/id_rsa'
 ssh $host 'chmod 444 ~/.ssh/id_rsa.pub'
 ssh $host 'chmod 600 ~/.ssh/authorized_keys2'
 
-
-ssh $host 'bash -ex' < system.sh
-ssh $host 'bash -ex' < ../ubuntu/zig.sh
-# ssh $host 'bash -ex' < ../macos/nats.sh
+#ssh $host 'bash -ex' <system.sh
+#ssh $host 'bash -ex' <../ubuntu/zig.sh
--- multipass/system.sh
+++ multipass/system.sh
@@ -3,62 +3,30 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-sudo -E apt-get install -y make build-essential autoconf
-sudo -E apt-get install -y curl net-tools unzip htop tree git fd-find jq ripgrep
-sudo -E apt-get install -y zsh emacs-nox
-#sudo snap install emacs --classic
-#sudo -E snap install go --classic
+sudo -E apt install -y curl net-tools unzip make build-essential \
+    zsh git fd-find snapd openssh-server htop tree exa \
+    jq bat fzf ripgrep \
+    linux-libc-dev liburing-dev cmake \
+    linux-tools-common linux-tools-generic linux-tools-$(uname -r) \
+    gdb hyperfine
 
-# Ruby
-#sudo -E apt-get install -y ruby-full
-#sudo gem install solargraph
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    sudo -E usermod --shell /usr/bin/zsh ubuntu
+    cd ~
+    ln -s ~/home/code/dot/shell/zshrc .zshrc
+    ln -s ~/home/code/dot/shell/bash_aliases .bash_aliases
+    ln -s ~/home/code/dot/shell/gitconfig .gitconfig
+    ln -s ~/home/code/dot/shell/gitignore .gitignore
+    mkdir -p ~/src
 
-sudo -E usermod --shell /usr/bin/zsh ubuntu
-cd ~
-ln -s ~/home/code/dot/shell/zshrc        .zshrc
-ln -s ~/home/code/dot/shell/bash_aliases .bash_aliases
-ln -s ~/home/code/dot/shell/gitconfig    .gitconfig
-ln -s ~/home/code/dot/shell/gitignore    .gitignore
-mkdir -p ~/src
-
-# zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-export ZSH_CUSTOM=~/.oh-my-zsh/custom
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-
-# lsd tool
-# https://github.com/Peltoche/lsd/releases
-cd ~/src
-version=0.23.1
-wget -q https://github.com/Peltoche/lsd/releases/download/"$version"/lsd_"$version"_arm64.deb
-sudo dpkg -i lsd_"$version"_arm64.deb
-rm lsd_"$version"_arm64.deb
-
-# # performance monitoring tools
-# sudo -E apt-get install -y linux-tools-common linux-tools-generic linux-tools-`uname -r`
-# # classic
-# cd ~/src
-# git clone git@github.com:brendangregg/FlameGraph.git
-
-
-# #  building perf from source
-# cd ~/src
-# sudo apt-get install -y flex bison
-
-# sudo apt-get source linux-image-unsigned-$(uname -r)
-# cd linux-5.15.0/tools/perf
-# make
-
-# # a onda jos za flamegraph: https://github.com/spiermar/d3-flame-graph#input-format
-# sudo apt install -y nodejs npm
-
-# ovo mi je trebalo da buildam libressl
-# sudo apt-get install -y automake libtool libtool-bin
-
+    # zsh
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    export ZSH_CUSTOM=~/.oh-my-zsh/custom
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+fi
 
 # skip "The authenticity of host 'github.com can't be established."
 # on first git pull
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-keyscan github.com >>~/.ssh/known_hosts
