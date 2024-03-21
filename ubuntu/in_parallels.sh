29a99a050866ad1ec3d90b3b3e57aff164629ca9
Igor Anić <igor.anic@gmail.com>
2024-03-21 19:32:22 +0100

    keyboard replace caps lock with control
    instead of swapping

--- ubuntu/in_parallels.sh
+++ ubuntu/in_parallels.sh
@@ -2,13 +2,11 @@
 
 # Keyboard
 xset r rate 300 30
-setxkbmap -model "macbook79" -layout us  -option "ctrl:swapcaps"
+# Replaces caps lock with control, use "ctrl:swapcaps" to swap capslock and control
+setxkbmap -model "macbook79" -layout us  -option "ctrl:nocaps"
 
 # Monitor resolution
 xrandr --output Virtual-1 --mode 6720x3780
-
-# Removes icon (keyboard switch) EN from top right
-# killall ibus-ui-gtk3
 
 # Set wallpaper
 feh --bg-scale ~/host/Pictures/Big\ Sur\ Road.jpg
