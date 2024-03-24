1675ad973a03f94383bfe07de4b6d6ffc5f2265e
Igor Anić <igor.anic@gmail.com>
2024-03-24 18:11:55 +0100

    fix right alt key on thinkpad

--- ubuntu/in_parallels.sh
+++ ubuntu/in_parallels.sh
@@ -6,8 +6,11 @@ xset r rate 300 30
 # Replaces caps lock with control, use "ctrl:swapcaps" to swap capslock and control
 case $(hostname) in
   thinkpad)
-      xmodmap ~/.config/dot/ubuntu/Xmodmap
       setxkbmap -layout us -option "ctrl:nocaps,altwin:swap_alt_win"
+      xmodmap ~/.config/dot/ubuntu/Xmodmap
+      # map PrtSc to alt
+      # xmodmap -pke usefull for showing all mappings
+      xmodmap -e 'keycode 107 = Alt_R Meta_R Alt_R Meta_R'
       ;;
   io)
       setxkbmap  -layout "us" -model "macbook79"  -option "ctrl:nocaps"
@@ -25,5 +28,3 @@ feh --bg-scale ~/.config/dot/wallpaper/big_sur_road.jpg
 # Compton
 killall compton
 compton --config ~/.config/i3/compton.conf -b
-
-
