719df73a6d82b508eab23827cc4e0f9c54b1c87a
Igor Anić <igor.anic@gmail.com>
2024-03-26 00:10:15 +0100

    fixing keyboard ± key and right alt

--- ubuntu/in_parallels.sh
+++ ubuntu/in_parallels.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 
-# Keyboard
+# Compton, killing compton resets kyboard variant (mac) somehove
+# keep it before keyboard configuration
+# killall compton
+compton --config ~/.config/i3/compton.conf -b
+
+# Set wallpaper
+feh --bg-scale ~/.config/dot/wallpaper/big_sur_road.jpg
+
+# Keyboard repeat rate
 xset r rate 300 30
 
-# Replaces caps lock with control, use "ctrl:swapcaps" to swap capslock and control
+# Host dependent keyboard configs
 case $(hostname) in
   thinkpad)
+      # Replaces caps lock with control, use "ctrl:swapcaps" to swap capslock and control
       setxkbmap -layout us -option "ctrl:nocaps,altwin:swap_alt_win"
       xmodmap ~/.config/dot/ubuntu/Xmodmap
       # map PrtSc to alt
@@ -16,21 +25,20 @@ case $(hostname) in
       setxkbmap  -layout "us" -model "macbook79"  -option "ctrl:nocaps"
       # Monitor resolution
       xrandr --output Virtual-1 --mode 6720x3780
+      /usr/bin/prlcc &
       ;;
   hydra)
-      setxkbmap  -layout "us(mac)" -model "macbook79"  -option "ctrl:nocaps"
+      # Fix mouse cursor size
+      xrdb ~/.config/dot/ubuntu/hydra/Xresources
+      # Fix ~,§  map caps to to control
+      setxkbmap -layout "us(mac)" -model "macbook79" -option "ctrl:nocaps"
+      # Fix right alt, must be after setxkbmap
+      xmodmap .config/dot/ubuntu/hydra/Xmodmap
       ;;
   *)
       echo else
       ;;
 esac
-
-# Set wallpaper
-feh --bg-scale ~/.config/dot/wallpaper/big_sur_road.jpg
-
-# Compton
-killall compton
-compton --config ~/.config/i3/compton.conf -b
 
 # Barrier
 # Configuration is in /home/ianic/.local/share/barrier/.barrier.conf
