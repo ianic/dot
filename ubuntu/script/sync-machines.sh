972d277b705aadf9de0ad51545237647734975bb
Igor Anić <igor.anic@gmail.com>
2024-03-29 20:47:35 +0100

    update dcom conf

---
+++ ubuntu/script/sync-machines.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+echo macOS
+
+rsync -ai \
+    --exclude zig-cache --exclude zig-out \
+    --exclude build --exclude .git \
+    --exclude tmp --exclude .DS_Store \
+    ~/Code/zig/ callisto:/Users/ianic/code/$(hostname)/zig
+
+rsync -ai \
+    --exclude zig-cache --exclude zig-out \
+    --exclude build --exclude .git \
+    --exclude tmp --exclude .DS_Store \
+    ~/Code/zig-fetch-test/ callisto:/Users/ianic/code/$(hostname)/zig-fetch-test
+
+
+# echo Windows
+
+# # sudo mount -t cifs -o username=ianic,dir_mode=0777,file_mode=0777 //10.211.55.27/code /media/win_code
+
+# rsync -rDzi --exclude zig-cache --exclude zig-out --exclude build --exclude .git --exclude tmp --exclude .DS_Store \
+#     ~/zig/zig/ /media/win_code/zig-io
+
+
+# # rsync -rDvzv --exclude zig-global-cache-release --exclude zig-global-cache-master --exclude zig-cache --exclude zig-out  ~/zig/issues/fetch/ /media/win_code/issues/fetch
