569e574e37e9e6ed9e7c616bda122b3771c51346
Igor Anić <igor.anic@gmail.com>
2025-05-14 17:47:39 +0200

    add nixos configuration

---
+++ nixos/flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      #url = "github:nix-community/home-manager/release-24.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zls-overlay.url = "github:zigtools/zls";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:  let
+    system = "x86_64-linux";
+    # Overlays is the list of overlays we want to apply from flake inputs.
+    overlays = [
+      inputs.zig-overlay.overlays.default
+      (final: prev: {
+        zlspkgs = inputs.zls-overlay.packages.${system}.default;
+      })
+    ];
+  in {
+    # Configuration for nixos hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        { nixpkgs.overlays = overlays; }
+
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+
+        # make home-manager as a module of nixos
+        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            backupFileExtension = "backup";
+            users.ianic = import /home/ianic/.config/nixos/home.nix;
+          };
+          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+        }
+      ];
+    };
+  };
+}
