71c8197b1578b7cb62bca87f92f088af0d822814
Igor Anić <igor.anic@gmail.com>
2025-05-15 14:44:46 +0200

    modularize home.nix

---
+++ nixos/packages.nix
@@ -0,0 +1,139 @@
+{  pkgs,  config, lib, ...}: {
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    htop
+    fortune
+    zigpkgs.master
+    zlspkgs
+    git
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    emacs-gtk
+
+    # doom emacs
+    ripgrep
+    coreutils
+    fd
+    clang
+    # # da bi mogao buildati vterm u emacs-u
+    # # to je sada pokriveno s coreutils vjerujem
+    # gnumake
+    # cmake
+    # gcc
+    # libtool
+
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    neofetch
+    # nnn # terminal file manager
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    # ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # # networking tools
+    # mtr # A network diagnostic tool
+    # iperf3
+    # dnsutils  # `dig` + `nslookup`
+    # ldns # replacement of `dig`, it provide the command `drill`
+    # aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    # socat # replacement of openbsd-netcat
+    # nmap # A utility for network discovery and security auditing
+    # ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # # misc
+    # cowsay
+    # file
+    # which
+    # tree
+    # gnused
+    # gnutar
+    # gawk
+    # zstd
+    # gnupg
+
+    # # nix related
+    # #
+    # # it provides the command `nom` works just like `nix`
+    # # with more details log output
+    # nix-output-monitor
+
+    # # productivity
+    # hugo # static site generator
+    # glow # markdown previewer in terminal
+
+    # btop  # replacement of htop/nmon
+    # iotop # io monitoring
+    # iftop # network monitoring
+
+    # # system call monitoring
+    # strace # system call monitoring
+    # ltrace # library call monitoring
+    # lsof # list open files
+
+    # # system tools
+    # sysstat
+    # lm_sensors # for `sensors` command
+    # ethtool
+    # pciutils # lspci
+    # usbutils # lsusb
+  ];
+
+  programs.git = {
+    # package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "Igor Anić";
+    userEmail = "igor.anic@gmail.com";
+    aliases = {
+    };
+    extraConfig = {
+      color = {
+        ui = "auto";
+      };
+      pull = {
+        rebase = true;
+      };
+      core = {
+        excludesfile = "~/.gitignore";
+      };
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      nrs = "sudo nixos-rebuild switch --impure --flake /home/ianic/.config/dot/nixos#nixos";
+      d = "eza -l --icons";
+      ll = "ls -l";
+      e = "emacsclient -n"; # send file to emacs
+      doom = "~/.config/emacs/bin/doom";
+      zb = "zig build";
+      zbf = "zig build -Doptimize=ReleaseFast";
+      emacs-restart = "systemctl --user restart emacs";
+    };
+
+    # ... # Your zsh config
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+  };
+
+  programs.ghostty = {
+    enable = true;
+    settings =  {
+      theme = "GitHub-Light-High-Contrast";
+    };
+  };
+}
